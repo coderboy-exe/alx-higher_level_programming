a2f0016dd47ca05ab0b1c1f5b74689e51ab35c92
Uchechukwu Ottah <coderboy.exe@gmail.com>
2022-08-25 10:34:09 -0700

    hotfixx

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,11 +8,11 @@ if __name__ == "__main__":
     if (len(args) == 0):
         print('arguments.')
     elif (len(args) == 1):
-        print('argument: ', end='\n')
+        print('argument:')
         print('1: {:s}'.format(argv[1]))
 
     else:
-        print('arguments:', end='\n')
+        print('arguments:')
 
     for i in range(1, len(args)):
         print('{:d}: {:s}'.format(i + 1, (argv[i + 1])), end='\n')
