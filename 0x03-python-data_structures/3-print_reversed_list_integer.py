01b3ba891c3a281824860b52d1efa8fb726be4cd
Uchechukwu Ottah <coderboy.exe@gmail.com>
2022-08-29 08:49:00 -0700

    replace all "c" characters in  a string

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    for i in range(len(my_list)):
-        while i < len(my_list):
-            if i == len(my_list):
-                break
-            print('{:d}'.format(my_list[i-1]))
+    for i in range(len(my_list)-1, -1):
+        while i < (len(my_list)):
+            print('{:d}'.format(my_list[i - 1]))
             i -= 1
