08c72824b6cc1ccd0cf9f48d1096d02255be7b52
Uchechukwu Ottah <coderboy.exe@gmail.com>
2022-09-12 05:09:02 -0700

    refactor method of ptinting to stderr

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-from sys import stderr
+from sys import exc_info, stderr
 
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return (True)
 
-    except (TypeError, ValueError) as err:
-        stderr.write("Exception: {}\n".format(err))
+    except (TypeError, ValueError):
+        print("Exception: {}".format(exc_info()[1]), file=stderr)
         return (False)
--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-from sys import stderr
+from sys import exc_info, stderr
 
 def safe_function(fct, *args):
     try:
         return fct(*args)
-    except (IndexError, TypeError, ValueError, ZeroDivisionError) as err:
-        stderr.write("Exception: {}\n".format(err))
+    except (IndexError, TypeError, ValueError, ZeroDivisionError):
+        print("Exception: {}".format(exc_info()[1]), file=stderr)
         return None
