b4bffd745f641203288aa3613114f197534d251f
Uchechukwu Ottah <coderboy.exe@gmail.com>
2022-09-19 08:36:25 -0700

    initial commit

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""
+    Defines Rectangle class
+"""
+
+
+class Rectangle():
+    """
+        Initialize Rectangle class
+    """
+    def __init__(self, width=0, height=0):
+        """
+            Initial Params
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """
+            Property getter
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+            Property setter
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """
+            Property getter
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+            Property setter
+        """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        """
+            Computes area of rectangle
+        """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """
+            Computes perimeter
+        """
+        return (2 * (self.__width + self__.height))
