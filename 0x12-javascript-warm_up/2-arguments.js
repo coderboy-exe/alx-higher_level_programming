cdb7ee815b76276abc668422af303d43f9f9b7b5
coderboy-exe <coderboy.exe@gmail.com>
2022-11-28 14:16:06 +0000

    do not import the "process" module

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
 
-import { argv } from 'node:process';
-
-if (argv.length === 2) {
+if (process.argv.length === 2) {
 	console.log('No argument');
 }
-else if (argv.length === 3) {
+else if (process.argv.length === 3) {
 	console.log('Argument found');
 }
-else if (argv.length > 2) {
+else if (process.argv.length > 2) {
 	console.log('Arguments found');
 }
